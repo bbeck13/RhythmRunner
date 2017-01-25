d6569c6a84d627624cfe9e176814032947aef5a5
Joseph Arhar <josepharhar@gmail.com>
2017-01-25 15:03:24 -0800

    Added headers for State, Updater, and Renderer

---
+++ src/game_state/GameObject.h
@@ -0,0 +1,34 @@
+// Joseph Arhar
+
+#ifndef GAME_OBJECT_H_
+#define GAME_OBJECT_H_
+
+#include <memory>
+
+#include "Shape.h"
+
+// GameObjects are 3D entities which can be rendered
+class GameObject {
+ public:
+  GameObject(std::unique_ptr<Shape> model);
+  virtual ~GameObject();
+
+  std::unique_ptr<Shape> GetModel();
+
+  glm::vec3 GetPosition();
+  glm::vec3 GetDirection();
+  float GetScale();
+
+  void SetPosition(glm::vec3 position);
+  void SetDirection(glm::vec3 direction);
+  void SetScale(float scale);
+
+ private:
+  std::unique_ptr<Shape> model;
+
+  glm::vec3 position;
+  glm::vec3 direction;
+  float scale;
+};
+
+#endif
