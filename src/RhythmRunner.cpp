8c937c106f46524386b2803c24947ba93425e78a
Nicolas Avila <avilanicolascgmail.com>
2017-01-25 23:03:24 -0800

    MORE \N

--- src/RhythmRunner.cpp
+++ src/RhythmRunner.cpp
@@ -1,53 +1,105 @@
 /**
+
 	*	RhythmRunner.cpp
+
 	*
+
 	*
+
 	*	For	now	this	is	just	a	bunch	of	import	statements	to	make	assert	the	dependencies
+
 	*	Braden	Beck	(bnbeck)
+
 	*
+
 	*/
 
+
+
 /*cpp	includes*/
+
 #include	<iostream>
 
+
+
 /*	Aquila	stuff*/
+
 #include	"aquila/global.h"
+
 #include	"aquila/source/WaveFile.h"
+
 #include	"aquila/tools/TextPlot.h"
+
 #include	"aquila/transform/AquilaFft.h"
+
 #include	"aquila/transform/OouraFft.h"
+
 #include	"aquila/source/generator/SineGenerator.h"
+
 #include	"aquila/transform/FftFactory.h"
 
+
+
 /*SFML*/
+
 #include	<SFML/Audio.hpp>
 
+
+
 /*GL	stuff	glew	glfw	etc..*/
+
 #define	GLEW_STATIC
+
 #include	<GL/glew.h>
+
 #include	<GLFW/glfw3.h>
 
+
+
 #include	"GLSL.h"
+
 #include	"Program.h"
+
 #include	"MatrixStack.h"
+
 #include	"Shape.h"
+
 #include	"Texture.h"
 
 
+
+
+
 //	value_ptr	for	glm
+
 #include	<glm/gtc/type_ptr.hpp>
+
 #include	<glm/gtc/matrix_transform.hpp>
 
+
+
 /*	to	use	glee	*/
+
 #define	GLEE_OVERWRITE_GL_FUNCTIONS
+
 #include	"glee.hpp"
+
+
 
 #include	"GameState.h"
 
+
+
 int	main(int	argc,	char	**argv)	{
+
 	std::cout	<<	"RhythmRunner"	<<	std::endl;
+
+
 
 	std::shared_ptr<GameState>	game_state;
 
+
+
 	return	EXIT_SUCCESS;
+
 }
--- src/game_renderer/GameRenderer.h
+++ src/game_renderer/GameRenderer.h
@@ -1,18 +1,35 @@
 //	Joseph	Arhar
 
+
+
 #ifndef	GAME_RENDERER_H_
+
 #define	GAME_RENDERER_H_
+
+
 
 #include	"game_state/GameState.h"
 
+
+
 class	GameRenderer	{
+
 	public:
+
 	GameRenderer();
+
 	~GameRenderer();
+
+
 
 	void	Render(std::shared_ptr<GameState>	game_state);
 
+
+
 	private:
+
 };
+
+
 
 #endif
