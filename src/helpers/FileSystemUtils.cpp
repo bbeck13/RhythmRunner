1d6dd0fecadfc26bf0120cf4fa72adf46d7fdd88
Joseph Arhar <josepharhar@gmail.com>
2017-01-26 21:22:06 -0800

    Compiles and runs on windows

--- src/helpers/FileSystemUtils.cpp
+++ src/helpers/FileSystemUtils.cpp
@@ -1,17 +1,24 @@
 // Alex Ottoboni
 
 #include <FileSystemUtils.h>
+
+#ifdef _WIN32
+#else
 #include <glob.h>
+#endif
 
 // Uses glob to list all files in path with pattern matching
 std::vector<std::string> FileSystemUtils::ListFiles(const std::string& pattern) {
+  std::vector<std::string> files;
+#ifdef _WIN32
+  // TODO(jarhar): implement this for windows
+#else
   glob_t glob_result;
   glob(pattern.c_str(), GLOB_TILDE, NULL, &glob_result);
-  std::vector<std::string> files;
   for (int i = 0; i < glob_result.gl_pathc;++i) {
     files.push_back(std::string(glob_result.gl_pathv[i]));
   }
   globfree(&glob_result);
+#endif
   return files;
 }
-
