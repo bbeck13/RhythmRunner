a4b2b57be9f9ed81b6c389d01826b64a27723fcc
Alex Ottoboni <aottobon@calpoly.edu>
2017-02-20 22:34:14 -0800

    Video Textures kind of working
    Dont store lots of textures
    
    Game Looks a lot better now
    
    Try to fix z fighting issues
    
    Made imgui gooier (#98)
    
    Updated cmake for linux user needs to define GLEW_DIR and GLFW_DIR in
    their environment
    
    Texture Loads like a shader
    
    Small cleanup changes
    
    Fix indents
    
    fixed error on exit
    
    Have a lovely day
    https://www.youtube.com/watch?v=DK4FTvaW_6Q
    
    added travis
    
    Made starting platform
    
    Added travis status in readme
    
    fixed jumping added infinite jumping in debug
    
    Commit to compare against master
    
    Made Sky into own type of scenery object
    
    Cleaned up some more things like trying to fix z fighting
    
    Fix segfault for non existent video textures
    
    Fix segfault for non existent video textures
    
    Rocks appear randomly in the level
    
    Texture Loads like a shader
    
    Small cleanup changes
    
    Fix indents
    
    Video Textures kind of working
    
    Dont store lots of textures
    
    Game Looks a lot better now
    
    Try to fix z fighting issues
    
    Made imgui gooier (#98)
    
    Updated cmake for linux user needs to define GLEW_DIR and GLFW_DIR in
    their environment
    
    Texture Loads like a shader
    
    Small cleanup changes
    
    Fix indents
    
    fixed error on exit
    
    Have a lovely day
    https://www.youtube.com/watch?v=DK4FTvaW_6Q
    
    added travis
    
    Made starting platform
    
    Added travis status in readme
    
    fixed jumping added infinite jumping in debug
    
    Commit to compare against master
    
    Made Sky into own type of scenery object
    
    Cleaned up some more things like trying to fix z fighting
    
    Fix segfault for non existent video textures
    
    Fix segfault for non existent video textures
    
    Fix leftover merge code
    
    Texture Loads like a shader
    
    Small cleanup changes
    
    Fix indents
    
    Video Textures kind of working
    
    Dont store lots of textures
    
    Game Looks a lot better now
    
    Try to fix z fighting issues
    
    Commit to compare against master
    
    Made Sky into own type of scenery object
    
    Cleaned up some more things like trying to fix z fighting
    
    Fix segfault for non existent video textures
    
    Fix segfault for non existent video textures
    
    Started rework of lighting system
    
    Added second rock

---
+++ src/game_state/PlainRock.h
@@ -0,0 +1,27 @@
+// Alex Ottoboni
+
+#ifndef __PLAINROCK_H__
+#define __PLAINROCK_H__
+#include "GameObject.h"
+#include "Shape.h"
+#include <glm/ext.hpp>
+
+#define PLAIN_ROCK_MESH "models/plain_rock.obj"
+
+namespace gameobject {
+class PlainRock : public GameObject {
+  public:
+    PlainRock();
+    PlainRock(glm::vec3 position, glm::vec3 scale);
+    ~PlainRock();
+
+    std::shared_ptr<Shape> GetModel() override;
+    ObjectType GetType() override;
+    SecondaryType GetSecondaryType() override;
+
+  private:
+    static std::shared_ptr<Shape> shape;
+    static bool isInitialized;
+};
+}
+#endif
