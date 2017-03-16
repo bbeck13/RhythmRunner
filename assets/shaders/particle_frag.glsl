76b817b131e352fa23f040b6ef671b03727cfcf4
Joseph Arhar <josepharhar@gmail.com>
2017-03-16 13:18:02 -0700

    made particles particulate

--- assets/shaders/particle_frag.glsl
+++ assets/shaders/particle_frag.glsl
@@ -5,8 +5,15 @@ out vec4 color;
 
 uniform sampler2D Texture0;
 
-void main()
-{
-    color = (texture(Texture0, TexCoords) * ParticleColor);
-   //color = ParticleColor;
+void main() {
+  //color = vec4(ParticleColor.xyz, texture(Texture0, TexCoords).w);
+  //float alpha = texture(Texture0, gl_PointCoord).r;
+
+  float alpha = texture(Texture0, TexCoords).r;
+  if (alpha < 0.001) {
+    discard;
+  }
+
+  color = vec4(ParticleColor.rgb, alpha);
+  //color = vec4(1, 1, 1, alpha);
 }
